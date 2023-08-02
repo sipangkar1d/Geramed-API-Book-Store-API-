17eee3afe7c8020521e8f3dd944d9e095ef38cd9
sipangkar1d <daniel222mu@gmail.com>
2023-08-02 20:07:35 +0700

    initial commit

---
+++ src/main/java/com/daniel/geramed/GeramedApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.geramed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeramedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeramedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/geramed/GeramedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.geramed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeramedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
